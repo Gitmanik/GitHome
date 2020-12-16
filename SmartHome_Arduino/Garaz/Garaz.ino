fe78936a3b802cbaeac5b02ea6fe6d1ef305856a
Paweł Reich <thegitman@outlook.com>
2020-12-16 19:05:22 +0100

    IRTerminal, new garage functions and fixes

--- SmartHome_Arduino/Garaz/Garaz.ino
+++ SmartHome_Arduino/Garaz/Garaz.ino
@@ -13,13 +13,18 @@ extern "C" {
 
 #define WIFI_SSID "***REMOVED***"
 #define WIFI_PASS "***REMOVED***"
-#define VERSION "2"
+#define VERSION "10"
 
 #define API_REPORT "http://***REMOVED***/api/report.php?id="
 #define API_UPDATE "http://***REMOVED***/api/update.php?id="
 #define INTERVAL 400
 
 int relay_pins[] = {5,4,0,2,14,12};
+
+#define INP 15
+#define ACTION 13
+
+#define DEBOUNCE 100
 
 void setup()
 {
@@ -29,6 +34,10 @@ void setup()
     digitalWrite(relay_pins[i], HIGH);
   }
 
+  pinMode(INP, INPUT);
+  
+  pinMode(ACTION, INPUT);
+  
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
@@ -39,6 +48,18 @@ void setup()
 }
 
 long previousMillis = 0;
+
+// Variables will change:
+int actionState = LOW;         // the current state of the output pin
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 100;    // the debounce time; increase if the output flickers
+
+
 void loop() {
   long currentMillis = millis();
   if (currentMillis - previousMillis >= INTERVAL) {
@@ -46,9 +67,43 @@ void loop() {
     worker();
   }
 
+  int reading = digitalRead(ACTION);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = currentMillis;
+  }
+
+  if ((currentMillis - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        actionState = HIGH;
+      }
+    }
+  }
+
+  // set the LED:
+  digitalWrite(ledPin, ledState);
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
 }
 WiFiClient client;
 HTTPClient http;
+
+
 
 void worker()
 {
@@ -86,5 +141,31 @@ void worker()
     http.end();
   }
   }
+  String api2 = String(API_REPORT);
+    api2 += wifi_station_get_hostname();
+    api2 += "_GARAZ";
+    api2 += "&version=";
+    api2 += VERSION;
+    api2 += "&data=";
+    api2 += digitalRead(INP);
   
+    if (http.begin(client, api2)) {
+      int httpCode = http.GET();
+      http.end();
+  }
+
+    String api3 = String(API_REPORT);
+    api3 += wifi_station_get_hostname();
+    api3 += "_ACTION";
+    api3 += "&version=";
+    api3 += VERSION;
+    api3 += "&data=";
+    api3 += actionState;
+
+    actionState = LOW;
+  
+    if (http.begin(client, api3)) {
+      int httpCode = http.GET();
+      http.end();
+  }
 }
--- SmartHome_Arduino/IRTerminal/IRTerminal.ino
+++ SmartHome_Arduino/IRTerminal/IRTerminal.ino
@@ -5,6 +5,10 @@
 #include <WiFiClient.h>
 #include <WiFiUdp.h>
 
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+
 #ifdef ESP8266
 extern "C" {
 #include "user_interface.h"
@@ -14,35 +18,54 @@ extern "C" {
 #define WIFI_SSID "***REMOVED***"
 #define WIFI_PASS "***REMOVED***"
 #define VERSION "1"
-#define RELAY_PIN 0
 #define API_REPORT "http://***REMOVED***/api/report.php?id="
 #define API_UPDATE "http://***REMOVED***/api/update.php?id="
-#define API_LOG    "http://***REMOVED***/api/log.php&id="
-#define INTERVAL 400
+
+IRrecv irrecv(2);
 
 void setup() {
-
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, HIGH);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("NO WIFI");
     delay(5000);
     ESP.restart();
   }
+
+  irrecv.enableIRIn();
+  Serial.println();
+  Serial.println("Ready");
 }
 
-long previousMillis = 0;
+decode_results results;
+
+#define COMMAND_MEMORY 16
+
+uint64_t commands[COMMAND_MEMORY];
+short pos = 0;
+long last = 0;
+
+
 void loop() {
-  long currentMillis = millis();
-  if (currentMillis - previousMillis >= INTERVAL) {
-    previousMillis = currentMillis;
-    worker();
+  if (irrecv.decode(&results)) {
+    if (pos < COMMAND_MEMORY - 1) {
+      commands[pos] = results.value;
+      pos++;
+      last = millis();
+      serialPrintUint64(results.value);
+      Serial.println();
+    }
+    irrecv.resume();  // Receive the next value
   }
 
+  if (pos > 0 && millis() > last + 1000) {
+      worker();
+      pos = 0;
+      memset(commands, 0, sizeof(commands));
+  }
+  delay(100);
 }
 WiFiClient client;
 HTTPClient http;
@@ -53,21 +76,20 @@ void worker()
   api_call += wifi_station_get_hostname();
   api_call += "&version=";
   api_call += VERSION;
+  api_call += "&data=";
+  for (int i = 0; i < pos; i++)
+  {
+      api_call += uint64ToString(commands[i]);
+      api_call += ";";
+  }
 
   if (http.begin(client, api_call)) {
     int httpCode = http.GET();
     if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
       String payload = http.getString();
-      if (payload == "true")
-      {
-        digitalWrite(RELAY_PIN, LOW);
-      }
-      if (payload == "false")
-      {
-        digitalWrite(RELAY_PIN, HIGH);
-      }
       if (payload == "UPDATE")
       {
+        Serial.println("Updating..");
         api_call = String(API_UPDATE);
         api_call += wifi_station_get_hostname();
         api_call += "&version=";
