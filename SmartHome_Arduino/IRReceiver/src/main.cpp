350e30009d92937a2977638d0828d9bab0afdfb5
Paweł Reich <thegitman@outlook.com>
2021-01-03 17:09:57 +0100

    IRReceiver

--- SmartHome_Arduino/IRReceiver/src/main.cpp
+++ SmartHome_Arduino/IRReceiver/src/main.cpp
@@ -1,13 +1,11 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
-#include <WiFiClient.h>
-#include <WiFiUdp.h>
 
 #include <IRremoteESP8266.h>
 #include <IRrecv.h>
 #include <IRutils.h>
+#include <Arduino.h>
 
 #ifdef ESP8266
 extern "C" {
@@ -17,26 +15,36 @@ extern "C" {
 
 #define WIFI_SSID "***REMOVED***"
 #define WIFI_PASS "***REMOVED***"
-#define VERSION "1"
-#define API_REPORT "http://***REMOVED***/api/report.php?id="
-#define API_UPDATE "http://***REMOVED***/api/update.php?id="
+#define VERSION "2"
 
 IRrecv irrecv(2);
 
+String pingString;
+String updateString;
+
+void worker();
+
 void setup() {
-  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("NO WIFI");
     delay(5000);
     ESP.restart();
   }
 
+  pingString = String("http://***REMOVED***/api/report.php?version=");
+  pingString += VERSION;
+  pingString += "&id=";
+  pingString += wifi_station_get_hostname();
+  pingString += "&data=";
+
+  updateString = String("http://***REMOVED***/api/update.php?version=");
+  updateString += VERSION;
+  updateString += "&id=";
+  updateString += wifi_station_get_hostname();
+
   irrecv.enableIRIn();
-  Serial.println();
-  Serial.println("Ready");
 }
 
 decode_results results;
@@ -44,8 +52,8 @@ decode_results results;
 #define COMMAND_MEMORY 16
 
 uint64_t commands[COMMAND_MEMORY];
-short pos = 0;
-long last = 0;
+unsigned short pos = 0;
+unsigned long last = 0;
 
 
 void loop() {
@@ -54,10 +62,8 @@ void loop() {
       commands[pos] = results.value;
       pos++;
       last = millis();
-      serialPrintUint64(results.value);
-      Serial.println();
     }
-    irrecv.resume();  // Receive the next value
+    irrecv.resume();
   }
 
   if (pos > 0 && millis() > last + 1000) {
@@ -72,11 +78,7 @@ HTTPClient http;
 
 void worker()
 {
-  String api_call = String(API_REPORT);
-  api_call += wifi_station_get_hostname();
-  api_call += "&version=";
-  api_call += VERSION;
-  api_call += "&data=";
+  String api_call = String(pingString);
   for (int i = 0; i < pos; i++)
   {
       api_call += uint64ToString(commands[i]);
@@ -89,12 +91,7 @@ void worker()
       String payload = http.getString();
       if (payload == "UPDATE")
       {
-        Serial.println("Updating..");
-        api_call = String(API_UPDATE);
-        api_call += wifi_station_get_hostname();
-        api_call += "&version=";
-        api_call += VERSION;
-        ESPhttpUpdate.update(client, api_call);
+        ESPhttpUpdate.update(client, updateString);
         ESP.restart();
       }
     }
