c4b56ac5e8e313526eb45d3c5c00aa0a21456e1c
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2023-12-27 00:06:02 +0100

    Testing build system

--- Server/api/pdo_connect.php
+++ Server/api/pdo_connect.php
@@ -1,6 +1,9 @@
 <?php
 try{
-    $pdo = new PDO("mysql:charset=utf8mb4;host=localhost;dbname=smartdom", "username", "password");
+    $username = getenv('DB_USERNAME');
+    $password = getenv('DB_PASSWORD');
+
+    $pdo = new PDO("mysql:charset=utf8mb4;host=localhost;dbname=smarthome", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
