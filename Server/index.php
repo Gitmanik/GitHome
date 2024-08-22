2c609d534ae25c61d8d168dc2b3741b1b5487fe4
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2024-08-22 21:54:16 +0200

    Server: ffmpeg and removal of php's logging.ini

--- Server/index.php
+++ Server/index.php
@@ -12,6 +12,12 @@ foreach ($all_devices_types as $device_type)
 {
     require $device_type;
 }
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 1);
+ini_set("log_errors", 1);
+ini_set('error_log', '/proc/1/fd/1');
+error_reporting(E_ALL);
+date_default_timezone_set('Europe/Warsaw');
 
 $path = parse_url($_SERVER['REQUEST_URI'])['path'];
 $path = ltrim($path, '/');
