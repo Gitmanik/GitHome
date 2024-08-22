2ebd2bf057b6455a88c81418f293302ffa82c714
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2024-08-23 00:51:08 +0200

    Server: Unified Panel asset names

--- Server/GitHome/Panel/index.php
+++ Server/GitHome/Panel/index.php
@@ -7,19 +7,19 @@
 	<link rel="stylesheet" href="/main.css">
 	<link rel="icon" type="image/png" href="/favicon.png">
 	
-	<script src="<?php GitPHP::static("index.js") ?>"></script>
+	<script src="<?php GitPHP::static("panel.js") ?>"></script>
 	<link rel="stylesheet" href="<?php GitPHP::static("panel.css") ?>">
 
 	<?php foreach (glob("../Handlers/CSS/*.css") as $css): ?>
 		<link rel="stylesheet" href="<?php GitPHP::static($css) ?>">
 	<?php endforeach;?>
-	
+
 </head>
 <body>
 	<p class="header"><a href="/config/">GitHome</a></p>
 	<ul class="devices"> 
 	
-	<?php foreach (GitHome::getDevices() as $dev): ?>
+	<?php foreach ($this->getDevices() as $dev): ?>
 
 	<li class="device border">
 		<?=$dev->render() ?>
